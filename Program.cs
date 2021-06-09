d556f4a54d0f67afcfa1cd298f7784ab2feababb
Carlos Carbonell López <carbonelldx89@gmail.com>
2021-06-08 23:02:24 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoubleConditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que captura una letra y te dice si es la letra A mayúscula o no");
+            Console.WriteLine("Coloque una letra: ");
+            var letter = Console.ReadLine();
+            if (letter == "A")
+            {
+                Console.WriteLine("La letra es una A mayúscula!!");
+            }
+            else
+            {
+                Console.WriteLine("La letra no es una A mayúscula :c");
+            }
+        }
+    }
+}
